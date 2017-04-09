8b885a743de1e0fac45e9c361907e5c8e3e14c9e
Wilson <wilsonlinumcp@gmail.com>
2017-04-09 16:24:45 -0400

    Add comments for customization

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
+// Change the periodInMinutes property to set time interval
 var alarm = chrome.alarms.create("myAlarm", { delayInMinutes: 0.05, periodInMinutes: 20 });
 
 chrome.alarms.onAlarm.addListener( function (alarm) {
   var notification = chrome.notifications.create(
     'take-a-break-notification', {
       type: 'basic',
-      iconUrl: 'icon.png',
+      iconUrl: 'icon.png', // Replace with your own image
+
+      // Customize either the notification title or message
       title: 'Take a break!',
       message: 'Look away from your computer screen and focus on a spot 20 feet away for 20 seconds.'
     },
